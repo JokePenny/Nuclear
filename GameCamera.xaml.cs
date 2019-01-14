10ae6b7ee92b1301eccbbe65b8989ac5118ccea3
Alex <S-total-war@mail.ru>
2019-01-15 00:59:47 +0700

    Add game map

--- GameCamera.xaml.cs
+++ GameCamera.xaml.cs
@@ -23,6 +23,33 @@ namespace Nuclear
         public GameCamera()
         {
             InitializeComponent();
+            InitOldGrid();
+        }
+
+        public void InitOldGrid()
+        {
+            int HeightMap = 20;
+            int WidthMap = 30;
+
+
+            Grid grid = new Grid();
+            grid.ShowGridLines = true;
+            for(int i = 0; i < HeightMap; i++)
+                grid.RowDefinitions.Add(new RowDefinition());
+            for(int i = 0; i < WidthMap; i++)
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            StackPanel testPanel = new StackPanel();
+            testPanel.Orientation = Orientation.Horizontal;
+            testPanel.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetRow(testPanel, 0);
+            Grid.SetColumn(testPanel, 0);
+            Grid.SetColumnSpan(testPanel, 3);
+
+            grid.Children.Add(testPanel);
+
+            this.Content = grid;
         }
     }
 }
