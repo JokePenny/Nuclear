6f24032439dfad37aca07a490fe6d4dd54921392
Alex <S-total-war@mail.ru>
2019-03-03 15:07:34 +0700

    Update launcher

--- src/NetworkRoom.xaml.cs
+++ src/NetworkRoom.xaml.cs
@@ -709,21 +709,96 @@ namespace Nuclear.src
 
         private void MoveMouseRoom_but(object sender, MouseEventArgs e)
         {
-            (sender as Border).Background = Brushes.DarkGreen;
+            if ((sender as Border).Background == Brushes.Black)
+            {
+                (sender as Border).Background = Brushes.DarkGreen;
+                if (Room.Visibility == Visibility.Collapsed)
+                    Room.Visibility = Visibility.Visible;
+            }
         }
 
         private void MoveLeaveRoom_but(object sender, MouseEventArgs e)
         {
-            (sender as Border).Background = Brushes.Black;
+            if((sender as Border).Background == Brushes.DarkGreen)
+            {
+                (sender as Border).Background = Brushes.Black;
+                if (Room.Visibility == Visibility.Visible && ExitRoom.IsEnabled == false)
+                    Room.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void MoveClickRoom_but(object sender, MouseEventArgs e)
+        {
+            (sender as Border).Background = Brushes.Green;
+            ConnectRoom.IsEnabled = true;
+            ConnectRoom.Opacity = 0.6;
+            Room.Visibility = Visibility.Visible;
         }
 
         private void CreateRoom_Click(object sender, MouseButtonEventArgs e)
         {
-            if(CreateRoom.Visibility == Visibility.Collapsed)
-                CreateRoom.Visibility = Visibility.Visible;
-            else
+            try
             {
-                CreateRoom.Visibility = Visibility.Collapsed;
+                if (CreateRoom.Visibility == Visibility.Collapsed)
+                {
+                    CreateWindow.IsEnabled = false;
+                    StatsPlayer.Margin = new Thickness(0, 0, 165, 0);
+                    CreateRoom.Visibility = Visibility.Visible;
+                }
+                else if (MapSelection.SelectedItem == null && ValuePlayers.Text == "" && NameRoom.Text == "")
+                    ChatTextBlock.Text += "\r\n Не все поля заполнены";
+                else
+                {
+                    StatsPlayer.Margin = new Thickness(0, 105, 5, 0);
+                    CreateWindow.IsEnabled = true;
+                    Border room = new Border();
+                    room.Height = 20;
+                    room.Background = Brushes.Black;
+                    room.BorderBrush = Brushes.Green;
+                    room.BorderThickness = new Thickness(2);
+                    room.Margin = new Thickness(0, 5, 0, 0);
+                    room.MouseMove += MoveMouseRoom_but;
+                    room.MouseLeave += MoveLeaveRoom_but;
+                    room.MouseLeftButtonDown += MoveClickRoom_but;
+
+                    WrapPanel wrap = new WrapPanel();
+
+                    TextBlock namemap = new TextBlock();
+                    namemap.Width = 200;
+                    namemap.Margin = new Thickness(0, 0, 196, 0);
+                    namemap.TextAlignment = TextAlignment.Center;
+                    namemap.Foreground = Brushes.Lime;
+                    namemap.FontSize = 12;
+                    namemap.FontFamily = new FontFamily(new Uri("pack://application:,,,/Nuclear"), "/data/fonts/#Fallout Display");
+                    namemap.Text = NameRoom.Text;
+
+                    TextBlock valPlayers = new TextBlock();
+                    valPlayers.Width = 136;
+                    valPlayers.TextAlignment = TextAlignment.Center;
+                    valPlayers.Foreground = Brushes.Lime;
+                    valPlayers.Margin = new Thickness(0, 0, 4, 0);
+                    valPlayers.FontSize = 12;
+                    valPlayers.FontFamily = new FontFamily(new Uri("pack://application:,,,/Nuclear"), "/data/fonts/#Fallout Display");
+                    valPlayers.Text = "0" + "/" + ValuePlayers.Text;
+
+                    TextBlock active = new TextBlock();
+                    active.Width = 130;
+                    active.TextAlignment = TextAlignment.Center;
+                    active.Foreground = Brushes.Lime;
+                    active.FontSize = 12;
+                    active.FontFamily = new FontFamily(new Uri("pack://application:,,,/Nuclear"), "/data/fonts/#Fallout Display");
+                    active.Text = "АКТИВЕН";
+
+                    wrap.Children.Add(namemap);
+                    wrap.Children.Add(valPlayers);
+                    wrap.Children.Add(active);
+                    room.Child = wrap;
+                    StackRoom.Children.Add(room);
+                    CreateRoom.Visibility = Visibility.Collapsed;
+                }
+            }
+            catch{
+                ChatTextBlock.Text += "\r\n Не все поля заполнены";
             }
         }
     }
